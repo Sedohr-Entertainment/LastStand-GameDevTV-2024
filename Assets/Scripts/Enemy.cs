c7929e98f13405b4e376b3b1f2e4b5581c686f90
xsedohrx <johnathan.claasen@hva.nl>
2024-05-28 09:13:52 +0200

    Enemies can now walk over walls
    Added Enemy models, enemy scripts and they can now walk up walls

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.AI.Navigation.Editor;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Enemy : MonoBehaviour
 {
+    private NavMeshAgent _navMeshAgent;
+    private int Health { get; set; }
+
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _moveSpeed;
+
+    [SerializeField] private bool _wallReached = false;
+    
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        MoveToDestination();
+    }
+
     public void TakeDamage(int amount)
+    {
+        Health -= amount;
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //TODO Kill enemy and Play death animation
+        Destroy(gameObject);
+    }
+
+    private void StartClimb()
     {
         
     }
+
+    private void MoveToDestination()
+    {
+        _navMeshAgent.SetDestination(_target.position);
+    }
+    
+
 }
